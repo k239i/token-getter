3f611bc2d11148d2aa137e4461a73a49d2a5c27c
sueqk <68449029+k239i@users.noreply.github.com>
2021-03-06 18:50:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('superagent');
 const {Client} = require('discord.js');
 const client = new Client();
 client.on('message', message => {
-  if(message.content.startsWith('t-')  return;
+  if(!message.content.startsWith('t-'))  return;
   const args = message.content.split(' ');
   var details = {
     email: args[1],
