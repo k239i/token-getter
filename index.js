224b66d7ef41359f64e640583ffcb582d92db923
sueqk <68449029+k239i@users.noreply.github.com>
2021-03-06 17:55:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const request = require('superagent');
-var details = {
-  email: 'zannko@woko.cow',
-  password: 'zannkowo'
-};
-request
-.post('https://discord.com/api/auth/login')
-.send(details)
-.end(function(err, res) {
+//h
+const {Client} = require('discord.js');
+const client = new Client();
+client.on('message', message => {
+  const args = message.content.split(' ');
+  var details = {
+    email: 'zannko@woko.cow',
+    password: 'zannkowo'
+  };
+  request
+  .post('https://discord.com/api/auth/login')
+  .send(details)
+  .end(function(err, res) {
   console.log(err)
   var dat = res.body;
   console.log(dat.token);
 });
-//h
